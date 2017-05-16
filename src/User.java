1a1bb1b918e728583181ecc73bfe2f5e97489e71
George-PC\George <dai16080@uom.edu.gr>
2017-05-16 18:11:52 +0300

    no message

---
+++ src/User.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.sql.*;
+
+
+public class User implements Serializable{
+
+	protected String user_name;
+	protected String password;
+	protected String first;
+	protected String last;
+	
+	
+	
+	public User(String user_name, String password, String first, String last) {
+		this.user_name = user_name;
+		this.password  = password;
+		this.last = last;
+		this.first = first;
+		
+	}
+	public void save_User(){
+		Connection conn = null ;
+		try {
+			
+			conn = User.getConnection();
+			
+		}
+		catch (Exception e) {
+			
+			System.out.println("Could not connect to database");
+						
+		}
+		
+		try{
+			
+			File file = new File("temp.bin");
+			FileOutputStream fout = new FileOutputStream(file);
+			ObjectOutputStream oos = new ObjectOutputStream(fout);
+			
+			oos.close();
+			fout.close();
+			
+			InputStream inputStream = new FileInputStream(new File(file.getAbsolutePath()));
+			 
+			String sql = "UPDATE users set arxeiaki = ? where user_name=" + this.user_name;
+			 
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setBlob(1, inputStream);
+			statement.executeUpdate();
+			inputStream.close();
+		}catch (Exception e){
+			
+			System.out.println("Problem in file uploading");
+		}
+		
+		
+	}
+
+	public static Connection getConnection() throws Exception{
+		try{
+			String driver = "com.mysql.jdbc.Driver";
+			Class.forName(driver);
+			
+			String url = "jdbc:mysql://snf-517490.vm.okeanos.grnet.gr/Ayeras";
+									//(IP address)
+			String username = "Ayeras";
+			String password = "giwrgos";
+			
+			Connection conn = DriverManager.getConnection(url,username,password);
+			System.out.println("Connected");
+			return conn;
+		} catch (Exception e ){
+			System.out.println(e);
+			return null;	
+		}
+	}
+	
+
+	
+}
