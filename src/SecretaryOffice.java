87387c29ab3c4be4266743e52b70c787175d1f85
George-PC\George <dai16080@uom.edu.gr>
2017-05-20 17:30:47 +0300

    Πραγματοποίηση εισόδου στο "Δημιουργία χρήστη" μέσω του Interface της Γραμματείας.

--- src/SecretaryOffice.java
+++ src/SecretaryOffice.java
@@ -1,71 +1,62 @@
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class SecretaryOffice {
+public class SecretaryOffice extends JFrame implements ActionListener{
 
-	private JFrame frmSecretarysOffice;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SecretaryOffice window = new SecretaryOffice();
-					window.frmSecretarysOffice.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	JButton btnAnoigmaKartelas ;
+	JButton btnDhmiourgiasXrhsth ;
+	JButton btnDoctorSearch ;
+	JButton btnPatientSearch;
+	JButton btnDisconnect ;
+	
+	
+	
+	public SecretaryOffice(){
+	
+		this.setResizable(false);
+		this.setTitle("Secretary's office");
+		this.setBounds(100, 100, 600, 400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.getContentPane().setLayout(null);
+		
+		btnAnoigmaKartelas = new JButton("’νοιγμα καρτέλας ασθενούς");
+		btnAnoigmaKartelas.setBounds(190, 70, 220, 23);
+		btnAnoigmaKartelas.addActionListener(this);
+		this.getContentPane().add(btnAnoigmaKartelas);
+		
+		btnDhmiourgiasXrhsth = new JButton("Δημιουργία Χρήστη");
+		btnDhmiourgiasXrhsth.setBounds(190, 110, 220, 23);
+		btnDhmiourgiasXrhsth.addActionListener(this);
+		this.getContentPane().add(btnDhmiourgiasXrhsth);
+		
+		btnDoctorSearch = new JButton("Αναζήτηση Γιατρών");
+		btnDoctorSearch.setBounds(190, 150, 220, 23);
+		btnDoctorSearch.addActionListener(this);
+		this.getContentPane().add(btnDoctorSearch);
+		
+		btnPatientSearch = new JButton("Αναζήτηση Αθενών");
+		btnPatientSearch.setBounds(190, 190, 220, 23);
+		btnPatientSearch.addActionListener(this);
+		this.getContentPane().add(btnPatientSearch);
+		
+		btnDisconnect = new JButton("Αποσύνδεση");
+		btnDisconnect.setBounds(240, 240, 120, 23);
+		this.getContentPane().add(btnDisconnect);
+		this.setVisible(true);
 	}
+		
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==btnDhmiourgiasXrhsth){
+			System.out.println("NAI RE PAOKARA");
+			this.dispose();
+			new ΔημιουργίαΧρήστη();
+		}
 
-	/**
-	 * Create the application.
-	 */
-	public SecretaryOffice() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmSecretarysOffice = new JFrame();
-		frmSecretarysOffice.setResizable(false);
-		frmSecretarysOffice.setTitle("Secretary's office");
-		frmSecretarysOffice.setBounds(100, 100, 600, 400);
-		frmSecretarysOffice.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmSecretarysOffice.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("\u0394\u03B7\u03BC\u03B9\u03BF\u03C5\u03C1\u03B3\u03AF\u03B1 \u03BA\u03B1\u03C1\u03C4\u03AD\u03BB\u03B1\u03C2 \u03B1\u03C3\u03B8\u03B5\u03BD\u03BF\u03CD\u03C2");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton.setBounds(190, 70, 220, 23);
-		frmSecretarysOffice.getContentPane().add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("\u0394\u03B7\u03BC\u03B9\u03BF\u03C5\u03C1\u03B3\u03AF\u03B1 \u03A7\u03C1\u03AE\u03C3\u03C4\u03B7");
-		btnNewButton_1.setBounds(190, 110, 220, 23);
-		frmSecretarysOffice.getContentPane().add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("\u0391\u03BD\u03B1\u03B6\u03AE\u03C4\u03B7\u03C3\u03AE \u0393\u03B9\u03B1\u03C4\u03C1\u03CE\u03BD");
-		btnNewButton_2.setBounds(190, 150, 220, 23);
-		frmSecretarysOffice.getContentPane().add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("\u0391\u03BD\u03B1\u03B6\u03AE\u03C4\u03B7\u03C3\u03B7 \u0391\u03C3\u03B8\u03B5\u03BD\u03CE\u03BD");
-		btnNewButton_3.setBounds(190, 190, 220, 23);
-		frmSecretarysOffice.getContentPane().add(btnNewButton_3);
-		
-		JButton btnNewButton_4 = new JButton("\u0391\u03C0\u03BF\u03C3\u03CD\u03BD\u03B4\u03B5\u03C3\u03B7");
-		btnNewButton_4.setBounds(240, 240, 120, 23);
-		frmSecretarysOffice.getContentPane().add(btnNewButton_4);
 	}
 
 }
